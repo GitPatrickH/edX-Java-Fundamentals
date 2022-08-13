d59ab1af95ffd11129bd4c41bdf9d0db1245b5a8
GitPatrickH <patrick.hessler@outlook.de>
2022-08-13 12:50:57 +0200

    not sure

--- src/main/java/ControlFlow.java
+++ src/main/java/ControlFlow.java
@@ -2,21 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ControlFlow {
-    public static void main(String[] args){}
-    //Complete the code so that the program print only odd numbers between 0 and 100.
-    public static void onlyEvens(short start, short end){
-        List<Short> evens = new ArrayList<Short>();
-
-        for (short i = start; i < end; i++) {
-            if ((i & 1) == 0) {
-                evens.add(i);
-            }
-        }
-        for (Short even : evens){
-            System.out.println(/*add odd number here*/ +even);
-        }
-    }
-    public static void onlyEvens(){
+    public static List<Short> onlyEvens() {
         List<Short> evens = new ArrayList<Short>();
 
         for (short i = 1; i < 100; i++) {
@@ -24,8 +10,18 @@ public class ControlFlow {
                 evens.add(i);
             }
         }
-        for (Short even : evens){
-            System.out.println(/*add odd number here*/ +even);
+        return evens;
+    }
+
+    //Complete the code so that the program print only odd numbers between 0 and 100.
+    public static List<Short> onlyEvens(final short start, final short end) {
+        List<Short> evens = new ArrayList<Short>();
+
+        for (short i = start; i < end; i++) {
+            if ((i & 1) == 0) {
+                evens.add(i);
+            }
         }
+        return evens;
     }
 }
--- src/test/java/ControlFlowTest.java
+++ src/test/java/ControlFlowTest.java
@@ -1,4 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ControlFlowTest {
-  
+
+    ControlFlow testSubject;
+
+    @Test
+    void isEmpty(){
+        List<Short> result = testSubject.onlyEvens((short) 1, (short)1);
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void onlyEvensTest(){
+        List<Short> result = testSubject.onlyEvens();
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(49, result.size());
+    }
 }
