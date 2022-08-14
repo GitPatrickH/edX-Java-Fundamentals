1eaa944fc3189faeaae58b946554f9cd4ce99ddd
GitPatrickH <patrick.hessler@outlook.de>
2022-08-14 10:16:50 +0200

    EJF-8 Added testNullInput to ControlFlowTest

--- src/test/java/ControlFlowTest.java
+++ src/test/java/ControlFlowTest.java
@@ -9,15 +9,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class ControlFlowTest {
 
     ControlFlow testSubject;
+    @Test
+    void testNullInput(){
+        Assertions.assertThrows(NullPointerException.class, () -> testSubject.onlyEvens(null, null));
+    }
 
     @Test
-    void isEmpty(){
+    void testEmptyInput(){
         List<Short> result = testSubject.onlyEvens((short) 1, (short)1);
         Assertions.assertTrue(result.isEmpty());
     }
 
     @Test
-    void onlyEvensTest(){
+    void testOnlyEvens(){
         List<Short> result = testSubject.onlyEvens();
         Assertions.assertFalse(result.isEmpty());
         Assertions.assertEquals(49, result.size());
